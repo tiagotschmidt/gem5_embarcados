1c3f156b75aa8fef48f8c15402839e47dd9bd1a1
Tiago Schmidt <68293535+tiagotschmidt@users.noreply.github.com>
2024-05-03 07:31:52 -0300

    Adding scripts for benchmark automation.

---
+++ scripts/frankenstein_benchmark.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Loop 100 times
+for i in {1..100}; do
+	# Execute the commando
+	build/ARM/gem5.opt emb/st_sim.py -c emb_exemplo/md5/frankenstein --cpu ARM_A72
+
+	# Move and rename stats.txt
+	mv m5out/stats.txt ~/results/
+	cp ~/results/stats.txt ~/results/frankenstein_$i.txt # Add counter to filename
+	rm ~/results/stats.txt
+done
+
+echo "Done executing benchmark."
